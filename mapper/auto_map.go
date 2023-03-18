e1e463c9a6722726b0599146579a075e39bab567
badboy <287564883@qq.com>
2023-03-18 09:16:40 +0800

    修复proto和struct之间相互转换得问题

--- mapper/auto_map.go
+++ mapper/auto_map.go
@@ -2,7 +2,6 @@ package mapper
 
 import (
 	"github.com/mitchellh/mapstructure"
-	"google.golang.org/protobuf/proto"
 	"google.golang.org/protobuf/types/known/wrapperspb"
 	"reflect"
 )
@@ -14,88 +13,97 @@ func MapTo(input interface{}, out interface{}) error {
 	return err
 }
 
-// MapToProtoMessage 映射到Message
-func MapToProtoMessage(input interface{}, out proto.Message) error {
-	decoder, err := mapstructure.NewDecoder(protoConfig(out))
+// ProtoToStruct proto转结构体
+func ProtoToStruct(input interface{}, out interface{}) error {
+	config := &mapstructure.DecoderConfig{
+		DecodeHook: func(f, t reflect.Type, data interface{}) (interface{}, error) {
+			from := f.String()
+			to := t.String()
+			if from == "*wrapperspb.StringValue" && to == "string" {
+				source := data.(*wrapperspb.StringValue)
+				return source.GetValue(), nil
+			} else if from == "*wrapperspb.Int64Value" && to == "int64" {
+				source := data.(*wrapperspb.Int64Value)
+				return source.GetValue(), nil
+			} else if from == "*wrapperspb.Int32Value" && (to == "int" || to == "int32") {
+				source := data.(*wrapperspb.Int32Value)
+				return source.GetValue(), nil
+			} else if from == "*wrapperspb.UInt32Value" && to == "uint32" {
+				source := data.(*wrapperspb.UInt32Value)
+				return source.GetValue(), nil
+			} else if from == "*wrapperspb.UInt64Value" && to == "uint64" {
+				source := data.(*wrapperspb.UInt64Value)
+				return source.GetValue(), nil
+			} else if from == "*wrapperspb.BytesValue" && to == "[]byte" {
+				source := data.(*wrapperspb.BytesValue)
+				return source.GetValue(), nil
+			} else if from == "*wrapperspb.DoubleValue" && to == "float64" {
+				source := data.(*wrapperspb.DoubleValue)
+				return source.GetValue(), nil
+			} else if from == "*wrapperspb.FloatValue" && to == "float32" {
+				source := data.(*wrapperspb.FloatValue)
+				return source.GetValue(), nil
+			} else if from == "*wrapperspb.BoolValue" && to == "bool" {
+				source := data.(*wrapperspb.BoolValue)
+				return source.GetValue(), nil
+			}
+
+			return data, nil
+		},
+		Result:           out,
+		WeaklyTypedInput: true,
+	}
+	decoder, err := mapstructure.NewDecoder(config)
 	if err != nil {
 		return err
 	}
 	return decoder.Decode(input)
 }
 
-func protoHook(f, t reflect.Kind, data interface{}) (interface{}, error) {
-	switch f.String() {
-	case "string":
-		{
-			if t.String() == "ptr" {
-				return wrapperspb.String(data.(string)), nil
+// MapToProtoMessage 映射到Message
+func MapToProtoMessage(input interface{}, out interface{}) error {
+	config := &mapstructure.DecoderConfig{
+		DecodeHook: func(f, t reflect.Type, data interface{}) (interface{}, error) {
+			from := f.String()
+			to := t.String()
+			if data == nil {
+				return nil, nil
 			}
-			break
-		}
-	case "int":
-		{
-			if t.String() == "ptr" {
-				return wrapperspb.Int32(int32(data.(int))), nil
+			if from == "string" && to == "*wrapperspb.StringValue" {
+				return &wrapperspb.StringValue{Value: data.(string)}, nil
+			} else if from == "int64" && to == "*wrapperspb.Int64Value" {
+				source := data.(int64)
+				return source, nil
+			} else if (from == "int" || from == "int32") && to == "*wrapperspb.Int32Value" {
+				source := data.(int)
+				return source, nil
+			} else if from == "uint32" && to == "*wrapperspb.UInt32Value" {
+				source := data.(uint32)
+				return source, nil
+			} else if from == "uint64" && to == "*wrapperspb.UInt64Value" {
+				source := data.(uint64)
+				return source, nil
+			} else if from == "[]byte" && to == "*wrapperspb.BytesValue" {
+				source := data.([]byte)
+				return source, nil
+			} else if from == "float64" && to == "*wrapperspb.DoubleValue" {
+				source := data.(float64)
+				return source, nil
+			} else if from == "float32" && to == "*wrapperspb.FloatValue" {
+				source := data.(float32)
+				return source, nil
+			} else if from == "bool" && to == "*wrapperspb.BoolValue" {
+				source := data.(bool)
+				return source, nil
 			}
-			break
-		}
-	case "int64":
-		{
-			if t.String() == "ptr" {
-				return wrapperspb.Int64(data.(int64)), nil
-			}
-			break
-		}
-	case "bool":
-		{
-			if t.String() == "ptr" {
-				return wrapperspb.Bool(data.(bool)), nil
-			}
-			break
-		}
-	case "float64":
-		{
-			if t.String() == "ptr" {
-				return wrapperspb.Double(data.(float64)), nil
-			}
-			break
-		}
-	case "float32":
-		{
-			if t.String() == "ptr" {
-				return wrapperspb.Float(data.(float32)), nil
-			}
-			break
-		}
-	case "slice":
-		{
-			if t.String() == "ptr" {
-				return wrapperspb.Bytes(data.([]byte)), nil
-			}
-			break
-		}
-	case "uint32":
-		{
-			if t.String() == "ptr" {
-				return wrapperspb.UInt32(data.(uint32)), nil
-			}
-			break
-		}
-	case "uint64":
-		{
-			if t.String() == "ptr" {
-				return wrapperspb.UInt64(data.(uint64)), nil
-			}
-			break
-		}
-	}
-	println("数据类型:%s", f.String())
-	return data, nil
-}
-func protoConfig(d interface{}) *mapstructure.DecoderConfig {
-	return &mapstructure.DecoderConfig{
-		DecodeHook:       protoHook,
-		Result:           d,
+			return data, nil
+		},
+		Result:           out,
 		WeaklyTypedInput: true,
 	}
+	decoder, err := mapstructure.NewDecoder(config)
+	if err != nil {
+		return err
+	}
+	return decoder.Decode(input)
 }
