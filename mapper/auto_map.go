b3e8a9239b3e3ca855d38579b0b2c2a88f01e5d4
badboy <287564883@qq.com>
2023-03-18 11:28:38 +0800

    修复proto和struct之间相互转换得问题

--- mapper/auto_map.go
+++ mapper/auto_map.go
@@ -92,6 +92,7 @@ func MapToProtoMessageList[T interface{}](input interface{}, out interface{}) er
 func protoDecoderHook(f, t reflect.Type, data interface{}) (interface{}, error) {
 	from := f.String()
 	to := t.String()
+	log.Errorf("from类型:%s to类型:%s", from, to)
 	if data == nil {
 		return nil, nil
 	}
@@ -99,28 +100,28 @@ func protoDecoderHook(f, t reflect.Type, data interface{}) (interface{}, error) 
 		return &wrapperspb.StringValue{Value: data.(string)}, nil
 	} else if from == "int64" && to == "*wrapperspb.Int64Value" {
 		source := data.(int64)
-		return source, nil
+		return wrapperspb.Int64(source), nil
 	} else if (from == "int" || from == "int32") && to == "*wrapperspb.Int32Value" {
-		source := data.(int)
-		return source, nil
+		source := data.(int32)
+		return wrapperspb.Int32(source), nil
 	} else if from == "uint32" && to == "*wrapperspb.UInt32Value" {
 		source := data.(uint32)
-		return source, nil
+		return wrapperspb.UInt32(source), nil
 	} else if from == "uint64" && to == "*wrapperspb.UInt64Value" {
 		source := data.(uint64)
-		return source, nil
+		return wrapperspb.UInt64(source), nil
 	} else if from == "[]byte" && to == "*wrapperspb.BytesValue" {
 		source := data.([]byte)
-		return source, nil
+		return wrapperspb.Bytes(source), nil
 	} else if from == "float64" && to == "*wrapperspb.DoubleValue" {
 		source := data.(float64)
-		return source, nil
+		return wrapperspb.Double(source), nil
 	} else if from == "float32" && to == "*wrapperspb.FloatValue" {
 		source := data.(float32)
-		return source, nil
+		return wrapperspb.Float(source), nil
 	} else if from == "bool" && to == "*wrapperspb.BoolValue" {
 		source := data.(bool)
-		return source, nil
+		return wrapperspb.Bool(source), nil
 	}
 	return data, nil
 }
