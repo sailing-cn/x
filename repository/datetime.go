88136e2fa2f383a2270c661bb03cde5ab570f7b9
badboy <287564883@qq.com>
2023-05-10 14:26:21 +0800

    更新仓储 BaseModel

--- repository/datetime.go
+++ repository/datetime.go
@@ -45,6 +45,7 @@ func (t *Time) Scan(v interface{}) error {
 }
 
 type BaseModel struct {
-	CreationTime   Time `gorm:"creation_time;type:timestamp;default:current_timestamp" json:"creation_time"`
-	LastModifyTime Time `gorm:"last_modify_time;type:timestamp" json:"last_modify_time"`
+	CreationTime int64    `gorm:"column:creation_time;type:bigint;not null;comment:创建时间"` //创建时间
+	Revision     Revision `gorm:"column:revision;type:varbinary;not null;comment:乐观锁"`    //乐观锁
+	IsDeleted    bool     `gorm:"column:is_deleted;type:bit;not null;comment:逻辑删除"`       //逻辑删除
 }
