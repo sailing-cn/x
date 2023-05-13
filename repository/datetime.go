44ae45f7a226f9394c0550930116166833ec0371
badboy <287564883@qq.com>
2023-05-13 11:47:36 +0800

    Revision字段在默认创建之前附值

--- repository/datetime.go
+++ repository/datetime.go
@@ -45,7 +45,7 @@ func (t *Time) Scan(v interface{}) error {
 }
 
 type BaseModel struct {
-	CreationTime int64    `gorm:"column:creation_time;type:bigint;not null;comment:创建时间"` //创建时间
-	Revision     Revision `gorm:"column:revision;type:varbinary;not null;comment:乐观锁"`    //乐观锁
-	IsDeleted    bool     `gorm:"column:is_deleted;type:bit;not null;comment:逻辑删除"`       //逻辑删除
+	CreationTime int64     `gorm:"column:creation_time;type:bigint;not null;comment:创建时间"` //创建时间
+	Revision     *Revision `gorm:"column:revision;type:varbinary;not null;comment:乐观锁"`    //乐观锁
+	IsDeleted    bool      `gorm:"column:is_deleted;type:bit;not null;comment:逻辑删除"`       //逻辑删除
 }
