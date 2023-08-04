71fffb236cb26cf748b0df40694a52f2efa625ba
badboy <287564883@qq.com>
2023-08-04 17:47:57 +0800

    1

--- convert/convert.go
+++ convert/convert.go
@@ -1,6 +1,11 @@
 package convert
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"strconv"
+	"strings"
+	"time"
+)
 
 func ToString(source interface{}) string {
 	if source == nil {
@@ -26,4 +31,35 @@ func ToBytes(in interface{}) (buf []byte) {
 		return []byte{}
 	}
 	return buf
+}
+
+// ToTimestamp 将指定格式的时间转换成时间戳 2022-03-11 11-30-50   03091545    0803062323
+func ToTimestamp(str string) int64 {
+	var result int64 = 0
+	if strings.Contains(str, "-") || strings.Contains(str, " ") {
+		str = strings.ReplaceAll(str, " ", "-")
+	} else {
+		if len(str) == 8 {
+			str = str + "00"
+		}
+		for i := 0; i < len(str); i += 3 {
+			str = str[:i] + "-" + str[i:]
+		}
+		str = strconv.Itoa(time.Now().Year()) + str
+	}
+
+	strFormatTimeList := strings.Split(str, "-")
+	if len(strFormatTimeList) == 6 {
+		year, err := strconv.Atoi(strFormatTimeList[0])
+		month, err := strconv.Atoi(strFormatTimeList[1])
+		day, err := strconv.Atoi(strFormatTimeList[2])
+		hour, err := strconv.Atoi(strFormatTimeList[3])
+		min, err := strconv.Atoi(strFormatTimeList[4])
+		sec, err := strconv.Atoi(strFormatTimeList[5])
+		if err != nil {
+			return result
+		}
+		result = time.Date(year, time.Month(month), day, hour, min, sec, 0, time.Local).Unix()
+	}
+	return result
 }
